925da9c68a1706042bf1837463f58244e17ee33a
gnios <eugenio00@gmail.com>
2015-08-13 16:59:10 -0300

    Commit inicial

---
+++ Desafio/App_Start/BundleConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Desafio
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery/jquery-{version}.js"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr/modernizr-*"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                      "~/Scripts/knockout/knockout-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/plugins").Include(
+                      "~/Scripts/jq-dropdown/jquery.dropdown.min.js",
+                      "~/Scripts/uikit/uikit.min.js",
+                      "~/Scripts/blackhole/main.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/css")
+                .Include("~/Content/css/site.css"));
+
+
+
+
+        }
+    }
+}
---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        webfont: {
+            icons: {
+                src: 'Desafio/Content/scss/typos/icons/svg/*.svg',
+                dest: 'Desafio/Content/css/fonts/',
+                destCss: 'Desafio/Content/scss/typos/icons',
+                options: {
+                    font: 'icons',
+                    stylesheet: 'scss',
+                    hashes: false,
+                    relativeFontPath: '../css/fonts',
+                    template: 'Desafio/Content/scss/typos/icons/templates/icon.css',
+                    templateOptions: {
+                        baseClass: 'icon',
+                        classPrefix: 'icon-',
+                        mixinPrefix: 'icon-mixin-'
+
+                    }
+                }
+            }
+        },
+        sass: {
+            // options: {
+            // includePaths: ['bower_components/foundation/scss']
+            // },
+            dist: {
+                options: {
+                    outputStyle: 'formated',
+                    sourceMap: true,
+                },
+                files: {
+                    'Desafio/Content/css/site.css': 'Desafio/Content/scss/site.scss'
+                }
+            }
+        },
+
+        watch: {
+            grunt: {
+                options: {
+                    reload: true
+                },
+                files: ['Gruntfile.js']
+            },
+            icons: {
+                files: 'Desafio/Content/scss/typos/icons/svg/*.svg',
+                tasks: ['webfont:icons']
+            },
+            sass: {
+                files: 'Desafio/Content/scss/**/*.scss',
+                tasks: ['sass']
+            }
+
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-webfont');
+
+    grunt.registerTask('build', ['sass']);
+    grunt.registerTask('default', ['build', 'watch']);
+}
